d650f6ccc60320f5aca41c23010df45a2ad30767
danielgil <danistcruz@gmail.com>
2022-07-01 00:43:07 -0500

    Se corrige el test de enviarCorreoPedido

--- domain/src/main/java/co/com/example/logisticaproductos/cliente/Contrato.java
+++ domain/src/main/java/co/com/example/logisticaproductos/cliente/Contrato.java
@@ -2,7 +2,6 @@ package co.com.example.logisticaproductos.cliente;
 import co.com.example.logisticaproductos.cliente.values.ContratoId;
 import co.com.example.logisticaproductos.cliente.values.Detalle;
 import co.com.sofka.domain.generic.Entity;
-
 public class Contrato extends Entity<ContratoId> {
     private Detalle detalle;
 
